66c2f16ec96d97bf20bdf9ae78ca3e6a79fbaa24
joeytall <joeytall.liu@gmail.com>
2014-05-30 08:54:50 -0700

    End of Chapter 9 with Playtime finished
    Change-Id: I85dda6f47205406225a5ce3fad432f6b91499f0f

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,15 @@
 class StoreController < ApplicationController
+  def increment_counter
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      session[:counter] += 1
+    end
+  end
+
   def index
+    @counter = increment_counter
     @products = Product.order(:title)
   end
+
 end
